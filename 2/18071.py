7b3d89de127c668485793bf126fa7223afb914a6
Андрей Боев <a.boev.it@gmail.com>
2023-10-05 19:40:48 +0300

    Задание 2

---
+++ 2/18071.py
@@ -0,0 +1,9 @@
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                f = (x and not(y)) or (y == z) or not(w)
+                # Т.к. функция в таблице равно нулю, берём обратное значение функции (not)
+                if not(f):
+                    print(x, y, z, w)
